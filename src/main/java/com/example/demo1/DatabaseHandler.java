4e87d5305c5054c76e374815f70258eae916417d
Kirill584 <108976942+Kirill584@users.noreply.github.com>
2022-07-12 06:22:43 +0300

    цитата+окно пользователя+окно добавления цитаты

--- src/main/java/com/example/demo1/DatabaseHandler.java
+++ src/main/java/com/example/demo1/DatabaseHandler.java
@@ -50,4 +50,44 @@ public class DatabaseHandler {
 
         return ResSet;
     }
+
+    public ResultSet getQuotes(Quotes quotes){
+        ResultSet ResSet = null;
+
+        String select = "SELECT * FROM " + Const.QUOTES_TABLE + " WHERE " + Const.QUOTES_QUOTES + "=? AND " + Const.QUOTES_TEACHER + "=? AND " + Const.QUOTES_SUBJECT + "=? AND " + Const.QUOTES_DATE + "=? AND " + Const.QUOTES_USER_ID + "=?";
+
+        try {
+            PreparedStatement prSt = getconnection().prepareStatement(select);
+            prSt.setString(1, quotes.getQuotes());
+            prSt.setString(2, quotes.getTeacher());
+            prSt.setString(3, quotes.getSubject());
+            prSt.setString(4, quotes.getDate());
+            prSt.setString(5, quotes.getUser_id());
+
+            ResSet = prSt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return ResSet;
+    }
+    public void signUpQuotes(Quotes quotes){
+        String insert = "INSERT INTO" + " " + Const.QUOTES_TABLE + "(" + Const.QUOTES_QUOTES + "," + Const.QUOTES_TEACHER + "," + Const.QUOTES_SUBJECT + "," + Const.QUOTES_DATE + "," + Const.QUOTES_USER_ID +")" + "VALUES(?,?,?,?,?)";
+        try {
+            PreparedStatement prSt = getconnection().prepareStatement(insert);
+            prSt.setString(1, quotes.getQuotes());
+            prSt.setString(2, quotes.getTeacher());
+            prSt.setString(3, quotes.getSubject());
+            prSt.setString(4, quotes.getDate());
+            prSt.setString(5, quotes.getUser_id());
+
+            prSt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
